2d7f00b7fc8972f741163a46ebb62d3c61a25b21
Koen Verheyen <verheyen.koen@gmail.com>
2026-02-13 10:57:34 +0100

    build: changed formatter rules
    Now using double quotes and semicolons.

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,6 @@
-import { expect, test } from 'bun:test'
-import { greet } from '../src'
+import { expect, test } from "bun:test";
+import { greet } from "../src";
 
-test('should greet correctly', () => {
-	expect(greet('World')).toBe('Hello, World!')
-})
+test("should greet correctly", () => {
+	expect(greet("World")).toBe("Hello, World!");
+});
